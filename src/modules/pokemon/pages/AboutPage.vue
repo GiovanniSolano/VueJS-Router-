238ffe2b272d6e31f09d1da80a3f42ec5fe162a1
Giovanni Solano <giovannisolano@MacBook-Pro-de-Giovanni.local>
2021-11-21 13:49:20 -0600

    router

---
+++ src/modules/pokemon/pages/AboutPage.vue
@@ -0,0 +1,66 @@
+<template>
+    <h1>About Page</h1>
+    <hr>
+    <!-- <h2>{{name}}</h2> -->
+    <button @click="onChangeName">
+        Cambiar nombre
+    </button>
+</template>
+
+<script>
+export default {
+
+    data() {
+        return {
+            name: 'Giovanni'
+        }
+    },
+    methods: {
+        onChangeName(){
+            console.log('Se cambiara el nombre');
+            this.name = 'Solano'
+            // PETICIONES HTTP
+        }
+    },
+
+    beforeCreate() {
+        console.log('beforeCreate');
+    },
+    created() {
+        console.log('created');
+    },
+    beforeMount() {
+        console.log('beforeMount');
+    },
+    beforeUnmount() {
+        console.log('beforeUnmount');
+    },
+    mounted() {
+        console.log('mounted');
+    },
+    beforeUpdate() {
+        console.log('beforeUpdate');
+    },
+    updated() {
+        console.log('updated');
+    },
+    activated() {
+        console.log('activated');
+    },
+    deactivated() {
+        console.log('deactivated');
+    },
+    unmounted() {
+        console.log('unmounted');
+    },
+    errorCaptured() {
+        console.log('errorCaptured');
+    },
+    renderTracked() {
+        console.log('renderTracked');
+    },
+    renderTriggered() {
+        console.log('renderTriggered');
+    },
+}
+</script>
